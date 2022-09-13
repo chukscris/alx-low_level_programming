173a4be15520860d32abc1f647e7b04a6daad93f
chukscris <chukscrisbaba@gmail.com>
2022-09-12 21:42:41 -0700

    9-print_comb

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints all the numbers of base 16 in lowercase.
+ * main-Prints all the numbers of base 16 in lowercase.
  *
  * Return: Always 0.
  */
@@ -15,5 +15,5 @@ putchar((num % 10) + '0);
 for (letter = 'a'; letter <= 'f'; letter++)
 putchar(letter);
 putchar('\n');
-return (0);
+return(0);
 }
