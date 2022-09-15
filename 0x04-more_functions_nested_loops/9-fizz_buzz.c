339de31f064e74d8584d837bf35b8adc6eefea81
chukscris <chukscrisbaba@gmail.com>
2022-09-15 15:58:29 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,14 +4,14 @@
 *main - program that prints either number
 *or fizz or buzz or fizzBuzz
 *
-*
+* 
 *Return: returns 0
 */
 
 int main(void)
+
 {
 int num = 1;
-
 while (num++ < 100)
 {
 if ((num % 3 == 0) && (num % 5 == 0))
