0028c70cdd752dc1705ef4fa4bb23ca828acd1e0
chukscris <chukscrisbaba@gmail.com>
2022-09-23 17:16:04 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
-*print_number - prints an integer.
-*@n: integer
-*Return: Always 0
-*/
-
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
+ */
 void print_number(int n)
 {
 	unsigned int m, d, count;
