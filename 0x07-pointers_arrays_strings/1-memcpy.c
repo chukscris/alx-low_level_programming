cfda597ceeb18441fe4cb7c4c08d72537da7ca02
chukscris <chukscrisbaba@gmail.com>
2022-09-26 19:51:31 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,11 +8,11 @@
 *Return: point to copied memory block
 */
 
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	for (; i < n; i++)
+	for (i= 0; n > 0; i++, n--)
 	{
 		dest[i] = src[i];
 	}
