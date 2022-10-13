351c982cba4f23402b7f77e1f40213b63d1262f4
chukscris <chukscrisbaba@gmail.com>
2022-10-13 08:14:07 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -24,3 +24,17 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", operation(a, b));
+	return (0);
+}
